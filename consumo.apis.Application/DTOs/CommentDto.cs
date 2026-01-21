62d2f307b9e9dc8243bcc9c3b6eed621557db7c1
dejovi2305 <dejovi230593@gmail.com>
2026-01-21 00:15:23 -0500

    chore: Se agregan algunos Dtos.

---
+++ consumo.apis.Application/DTOs/CommentDto.cs
@@ -0,0 +1,10 @@
+namespace consumo.apis.Application.DTOs;
+
+public class CommentDto
+{
+    public int PostId { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+}
---
+++ consumo.apis.Application/DTOs/PostDto.cs
@@ -0,0 +1,9 @@
+namespace consumo.apis.Application.DTOs;
+
+public class PostDto
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+}
